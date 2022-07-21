7fff6b4018cd0cd8bd3b39e60e84139c20f5664c
Joseslabs <76791600+Joseslabs@users.noreply.github.com>
2022-07-21 01:39:57 +0100

    config3

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
-const publicPath = process.env.NODE_ENV === 'production' ? '/To-Do_List/' : '/'module.exports = {
- publicPath: publicPath,
+const publicPath = process.env.NODE_ENV === 'production' ? '/To-Do_List/' : '/'
+
+module.exports = {
+  publicPath: publicPath,
 }
